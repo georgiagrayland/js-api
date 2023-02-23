af0981a98996012c06e7828cce74103ab6e5fb64
georgiagrayland <georgiamayeva@live.co.uk>
2023-02-23 13:46:56 +0000

    call display status function in if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
     } else {
         throw new Error(data.error);
     }
